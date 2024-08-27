e6c01d2081203e2ba9dc43e157f36e9c9774a4cd
paulomonteiro3 <paulomonteirop22@gmail.com>
2024-08-27 15:01:29 -0300

    modal refatorado com class

--- js/script.js
+++ js/script.js
@@ -1,25 +1,34 @@
-import ScrollSuave from './modules/scroll-suave.js';
-import TabNav from './modules/tab-nav.js';
-import initModal from './modules/modal.js';
-import initTooltip from './modules/tooltip.js';
-import initDropdownMenu from './modules/dropdown-menu.js';
-import initMenuMobile from './modules/menu-mobile.js';
-import initFuncionamento from './modules/funcionamento.js';
-import initFetchAnimais from './modules/fetch-animais.js';
-import initFetchBitcoin from './modules/fetch-bitcoin.js';
-import initAnimacaoScroll from './modules/animacao-scroll.js';
-import Accordion from './modules/accordion.js';
+import ScrollSuave from "./modules/scroll-suave.js";
+import TabNav from "./modules/tab-nav.js";
+import Modal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initFuncionamento from "./modules/funcionamento.js";
+import initFetchAnimais from "./modules/fetch-animais.js";
+import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+import initAnimacaoScroll from "./modules/animacao-scroll.js";
+import Accordion from "./modules/accordion.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
-const accordion = new Accordion('[data-anime="accordion"] dt')
-accordion.init()
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
 
-const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
-tabNav.init()
+const tabNav = new TabNav(
+  '[data-tab="menu"] li',
+  '[data-tab="content"] section'
+);
+tabNav.init();
 
-initModal();
+const modal = new Modal(
+  '[data-modal="abrir"]',
+  '[data-modal="fechar"]',
+  '[data-modal="container"]'
+);
+modal.init();
+
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
@@ -27,5 +36,3 @@ initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
 initAnimacaoScroll();
-
-console.log('teste')
