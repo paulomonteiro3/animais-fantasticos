0e6e63cf011d901a29d8b1920c99f09fe080fdcf
paulomonteiro3 <paulomonteirop22@gmail.com>
2024-08-27 12:02:03 -0300

    erros corrigidos

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -2,30 +2,24 @@ export default class ScrollSuave {
   constructor(links, options) {
     this.linksInternos = document.querySelectorAll(links);
     if (options === undefined) {
-      this.options = { behavior: "smooth", block: "start" };
+      this.options = { behavior: 'smooth', block: 'start' };
     } else {
       this.options = options;
     }
-    this.scrollToSection = this.scrollToSection.bind(ScrollSuave);
+
+    this.scrollToSection = this.scrollToSection.bind(this);
   }
 
   scrollToSection(event) {
     event.preventDefault();
-    const href = event.currentTarget.getAttribute("href");
+    const href = event.currentTarget.getAttribute('href');
     const section = document.querySelector(href);
     section.scrollIntoView(this.options);
-
-    // forma alternativa
-    // const topo = section.offsetTop;
-    // window.scrollTo({
-    //   top: topo,
-    //   behavior: 'smooth',
-    // });
   }
 
   addLinkEvent() {
     this.linksInternos.forEach((link) => {
-      link.addEventListener("click", this.scrollToSection);
+      link.addEventListener('click', this.scrollToSection);
     });
   }
 
